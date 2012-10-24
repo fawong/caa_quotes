24ac19ec04515c2a338cc48bf57b23d9ec175ea2
Kenny Do <kennydo@berkeley.edu>
2012-10-24 13:12:40 -0700

    created setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='caa-quotes',
+      description="The Django project that powers the web interface to view the quotes collected by the #CAA quotes bot",
+      author="Kenny Do",
+      author_email="kedo@ocf.berkeley.edu",
+      packages=['caaquotes',
+                'caaquotes.quotes'],
+      data_files=[],
+      version='1.0.0',
+      long_description="""
+The Django project that powers the web interface to view the quotes collected by the quotes bot on #CAA, the IRC channel of Cal Animage Alpha, UC Berkeley's anime club
+                       """,
+      url='http://www.calanimagealpha.com',
+      classifiers=['Programming Language :: Python'],
+      )
