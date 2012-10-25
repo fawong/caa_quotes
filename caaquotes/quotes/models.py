4457c9e57765d19f8c66a3456b3132c0891caeff
Kenny Do <kennydo@berkeley.edu>
2012-10-25 12:06:01 -0700

    put linebreaks between quote lines

--- caaquotes/quotes/models.py
+++ caaquotes/quotes/models.py
@@ -1,9 +1,15 @@
+import re
+
 from django.db import models
 
 class Quote(models.Model):
     id = models.IntegerField(primary_key=True)
     quote = models.TextField()
     context = models.CharField(max_length=255)
+
+    def lines(self):
+        lines = re.split(r'(<\S+>)', self.quote)
+        return ["%s%s" % (x, y) for (x, y) in zip(lines[1::2], lines[2::2])]
 
     class Meta:
         db_table = 'quotes'
