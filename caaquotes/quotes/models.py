44f28d3e3872f5189f57fb424de4b04fab814d38
Nolan Lum <nol888@gmail.com>
2012-10-29 22:16:54 -0600

    Fix displaying the quote in the case in which the first line of text doesn't have a nick preceeding it.

--- caaquotes/quotes/models.py
+++ caaquotes/quotes/models.py
@@ -9,19 +9,11 @@ class Quote(models.Model):
     quote = models.TextField()
     context = models.CharField(max_length=255)
 
-    def has_regex(self, regex):
-        matched = re.search(regex, self.quote)
-        return matched is not None
-
-    def has_timestamps(self):
-        return self.has_regex(TIMESTAMP_REGEX)
-
-    def has_nicks(self):
-        return self.has_regex(NICK_REGEX)
-
     def lines(self):
-        splitted = re.split(LINE_PREFIX_REGEX, self.quote)[1:]
-        return map((lambda x,y: x + y), splitted[0::2], splitted[1::2])
+        splitted = re.split(LINE_PREFIX_REGEX, self.quote)
+        first = splitted.pop(0)
+        next = map((lambda x,y: x + y), splitted[0::2], splitted[1::2])
+        return next if not len(first) else [first] + next
 
     class Meta:
         db_table = 'quotes'
